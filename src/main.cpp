b2ae71d6c397d1c26a1696d348438171ec32d9a4
s223312 <s223312@dtu.dk>
2024-02-09 00:57:05 +0100

    Added textured and scaled Plane for the sphere

--- src/main.cpp
+++ src/main.cpp
@@ -22,8 +22,8 @@ double lastTime = glfwGetTime();
 int nbFrames = 0;
 
 // Camera
-float radius = 3.0f;
-float verticalAngle = 0.0f;
+float radius = 8.0f;
+float verticalAngle = 0.5f;
 float horizontalAngle = 0.0f;
 float cameraSpeed = 0.03f; // camera speed per frame
 glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, radius);
@@ -34,22 +34,38 @@ float farPlane = 100.0f;
 
 
 // MVP matrices
-glm::mat4 modelMatrix = glm::mat4(1.0f);    // Calculate model matrix
+// glm::mat4 modelMatrix = glm::mat4(1.0f);    // Calculate model matrix
 glm::mat4 viewMatrix = glm::lookAt(cameraPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f)); // Calculate view matrix
 glm::mat4 projectionMatrix = glm::perspective(glm::radians(fov), (float)SCR_WIDTH/(float)SCR_HEIGHT, nearPlane, farPlane); // Calculate projection matrix
 
 
 // TEST VERTICES
 //create sphere
-Sphere sphere(1.0f, 30, 30);
+Sphere sphere(1.5f, 30, 30);
+float planePositions[] = {
+    1.0f,  0.0f, 1.0f,
+    1.0f,  0.0f, -1.0f,
+    -1.0f, 0.0f, -1.0f,
+    -1.0f, 0.0f, 1.0f
+};
 
+float planeTextureCoords[] = {
+    1.0f, 1.0f,
+    1.0f, 0.0f,
+    0.0f, 0.0f,
+    0.0f, 1.0f
+};
 
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 2, 1,  // first Triangle
-        1, 2, 3   // second Triangle
-    };
+unsigned int planeIndices[] = {
+    0, 1, 3,
+    1, 2, 3
+};
 
-unsigned int VBO;
+unsigned int sphereVAO, sphereVBO, sphereEBO, sphereTBO, sphereTexture;
+unsigned int planeVAO, planeVBO, planeEBO, planeTBO, planeTexture;
+
+// MVP matrix locations
+unsigned int modelMatrixLoc, viewMatrixLoc, projectionMatrixLoc;
 
 
 // Registering a callback function that gets called each time the window is resized.
@@ -68,8 +84,134 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 // function declarations
 void CalculateFrameRate(GLFWwindow* window);
 void key_callback(GLFWwindow* window);
+unsigned int loadTexture(const char *path);
 // void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
+
+void setupPlane() {
+    // Generate the VAO
+    glGenVertexArrays(1, &planeVAO);
+    glBindVertexArray(planeVAO);
+
+    // Generate and set up the VBO
+    glGenBuffers(1, &planeVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(planePositions), planePositions, GL_STATIC_DRAW);
+
+    // Specify the vertex attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);  // Vertex positions
+
+    // Generate and set up the EBO
+    glGenBuffers(1, &planeEBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, planeEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(planeIndices), planeIndices, GL_STATIC_DRAW);
+
+    // Generate and set up the TBO
+    glGenBuffers(1, &planeTBO);
+    glBindBuffer(GL_ARRAY_BUFFER, planeTBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(planeTextureCoords), planeTextureCoords, GL_STATIC_DRAW);
+
+    // Specify the texture coordinates
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);  // Texture coordinates
+
+    // load texture
+    planeTexture = loadTexture("/Users/naglisnaslenas/Documents/DTU/Thesis/code/Refference/LearnOpenGL_Xcode/resources/textures/solid-grey.jpg");
+
+    // Unbind the VAO (optional)
+    glBindVertexArray(0);
+}
+
+void renderPlane() {
+    glm::mat4 model = glm::mat4(1.0f); // start with an identity matrix
+    model = glm::scale(model, glm::vec3(200.0f, 1.0f, 200.0f)); // apply scaling
+    glUniformMatrix4fv(modelMatrixLoc, 1, GL_FALSE, glm::value_ptr(model));
+    glBindVertexArray(planeVAO);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, planeTexture);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
+
+
+
+void setupSphere() {
+    // Generate the VAO
+    glGenVertexArrays(1, &sphereVAO);
+    glBindVertexArray(sphereVAO);
+
+    // Generate and set up the VBO
+    glGenBuffers(1, &sphereVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, sphereVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*sphere.getVertexCount(), sphere.getVertices(), GL_STATIC_DRAW);
+
+    // Specify the vertex attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);  // Vertex positions
+
+    // Generate and set up the EBO
+    glGenBuffers(1, &sphereEBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sphereEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int)* sphere.getIndexCount(), sphere.getIndices(), GL_STATIC_DRAW);
+
+    // Generate and set up the TBO
+    glGenBuffers(1, &sphereTBO);
+    glBindBuffer(GL_ARRAY_BUFFER, sphereTBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*sphere.getTexCoordCount(), sphere.getTexCoords(), GL_STATIC_DRAW);
+
+    // Specify the texture coordinates
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);  // Texture coordinates
+
+    sphereTexture = loadTexture("/Users/naglisnaslenas/Documents/DTU/Thesis/code/Refference/LearnOpenGL_Xcode/resources/textures/container.jpg");
+
+    // Unbind the VAO
+    glBindVertexArray(0);
+}
+
+void renderSphere() {
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(0.0f, sphere.radius, 0.0f)); // apply translation
+    glUniformMatrix4fv(modelMatrixLoc, 1, GL_FALSE, glm::value_ptr(model));
+    glBindVertexArray(sphereVAO);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, sphereTexture);
+    // glBindTexture(GL_TEXTURE_2D, texture);
+    glDrawElements(GL_TRIANGLES, sphere.getIndexCount(), GL_UNSIGNED_INT, 0);
+    
+}
+
+
+unsigned int loadTexture(const char *path)
+{
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load and generate the texture
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
+    std::cout << "Width: " << width << " Height: " << height << " nrChannels: " << nrChannels << std::endl;
+    if (data)
+    {
+        // generate the texture
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        // generate the mipmap
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    // free the image memory
+    stbi_image_free(data);
+    return texture;
+}
 
 int main(void)
 {
@@ -120,99 +262,28 @@ int main(void)
     // Registering the callback function on window resize to make sure OpenGL renders the image in the right size whenever the window is resized.
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     Shader ourShader("/Users/naglisnaslenas/Documents/DTU/Thesis/code/Refference/LearnOpenGL_Xcode/src/shaders/vertexShader.vs", "/Users/naglisnaslenas/Documents/DTU/Thesis/code/Refference/LearnOpenGL_Xcode/src/shaders/fragmentShader.fs");
+    
 
-    // Load texture
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // load and generate the texture
-    int width, height, nrChannels;
-    unsigned char *data = stbi_load("/Users/naglisnaslenas/Documents/DTU/Thesis/code/Refference/LearnOpenGL/resources/textures/container.jpg", &width, &height, &nrChannels, 0);
-    std::cout << "Width: " << width << " Height: " << height << " nrChannels: " << nrChannels << std::endl;
-    if (data)
-    {
-        // generate the texture
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-
-        // generate the mipmap
-        glGenerateMipmap(GL_TEXTURE_2D);
-        // set the texture wrapping/filtering options (on the currently bound texture object)
-        // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        // // Set the uniform variable in the shader to the texture unit
-        // glUniform1i(glGetUniformLocation(ourShader.ID, "tex"), 0);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    // free the image memory
-    stbi_image_free(data);
-
-    // Generating the vertex array object
-        unsigned int VAO;
-        glGenVertexArrays(1, &VAO);
-        glBindVertexArray(VAO);
-
-    // creating a Vertex buffer object
-        glGenBuffers(1, &VBO);
-        // binding the buffer type
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        // sending the data to the buffer
-        glBufferData(GL_ARRAY_BUFFER, sizeof(float)*sphere.getVertexCount(), sphere.getVertices(), GL_STATIC_DRAW);
-
-    // generating an element buffer object
-        unsigned int EBO;
-        glGenBuffers(1, &EBO);
-        // binding the buffer type
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        // sending the data to the buffer
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int)* sphere.getIndexCount(), sphere.getIndices(), GL_STATIC_DRAW);
-
-        // Setting the vertex attribute pointers for position data
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-
-    //creating a texture buffer object 
-        unsigned int TBO;
-        glGenBuffers(1, &TBO);
-        // binding the buffer type
-        glBindBuffer(GL_ARRAY_BUFFER, TBO);
-        // sending the data to the buffer
-        glBufferData(GL_ARRAY_BUFFER, sizeof(float)*sphere.getTexCoordCount(), sphere.getTexCoords(), GL_STATIC_DRAW);
-
-        // Link the texture coordinates to the vertex shader
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(1);
-
-
-
+    
     // Linking the shaders
         ourShader.use();
 
     // Enable depth test
         glEnable(GL_DEPTH_TEST);
     
+    setupPlane();
+    setupSphere();
+
+    modelMatrixLoc = glGetUniformLocation(ourShader.ID, "modelMatrix");
+    viewMatrixLoc = glGetUniformLocation(ourShader.ID, "viewMatrix");
+    projectionMatrixLoc = glGetUniformLocation(ourShader.ID, "projectionMatrix");
+
     /* Loop until the user closes the window */
+
     // The render loop
-
-    GLint modelMatrixLoc = glGetUniformLocation(ourShader.ID, "modelMatrix");
-    GLint viewMatrixLoc = glGetUniformLocation(ourShader.ID, "viewMatrix");
-    GLint projectionMatrixLoc = glGetUniformLocation(ourShader.ID, "projectionMatrix");
-
     while (!glfwWindowShouldClose(window))
     {
-        //check for key input (for camera movement)
-        key_callback(window);
         // MVP matrices to be used in the vertex shader
-        glUniformMatrix4fv(modelMatrixLoc, 1, GL_FALSE, glm::value_ptr(modelMatrix));
         glUniformMatrix4fv(viewMatrixLoc, 1, GL_FALSE, glm::value_ptr(viewMatrix));
         glUniformMatrix4fv(projectionMatrixLoc, 1, GL_FALSE, glm::value_ptr(projectionMatrix));
 
@@ -223,13 +294,15 @@ int main(void)
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        //set texture
-        glBindTexture(GL_TEXTURE_2D, texture);
+        // Drawing the sphere
+            renderSphere();
 
-        // Drawing the square
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, sphere.getIndexCount(), GL_UNSIGNED_INT, 0);
+        // Drawing the plane
+            renderPlane();
     
+
+        //check for key input
+        key_callback(window);
         //adjust point size
         // glPointSize(10.0f);
 
