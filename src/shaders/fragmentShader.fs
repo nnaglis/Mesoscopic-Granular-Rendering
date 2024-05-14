63b4516f2d7fe15d08cdfb043156187aac1f4d70
s223312 <s223312@dtu.dk>
2024-05-14 14:18:05 +0200

    Working version of FShader

--- src/shaders/fragmentShader.fs
+++ src/shaders/fragmentShader.fs
@@ -20,12 +20,14 @@ uniform vec3 eyePos;
 const float PI = 3.14159265359;
 
 // Predefined material properties
-uniform vec3 sigma_s = vec3(2.29, 2.39 , 1.97);
-uniform vec3 sigma_a = vec3(0.0030, 0.0034, 0.046);
+uniform vec3 sigma_s = vec3(2.19, 2.62, 3.00);
+uniform vec3 sigma_a = vec3(0.0021, 0.0041, 0.0071);
 uniform float g = 0.0;
-uniform float n_material = 1.3;
-uniform float roughness = 0.1;
-uniform vec3 reflectance = vec3(1.0 , 1.0, 1.0);
+uniform float n_material = 2.4;
+uniform float roughness = 0.0;
+
+
+uniform vec3 reflectance = vec3(1.0);
 
 // Material properties
 struct MaterialProperties {
@@ -43,8 +45,8 @@ struct MaterialProperties {
         // roughness
             // since im using the same roughness for x and y axis, it is a single value
         float roughness;
-        // amount of light that is reflected by the material
-        vec3 reflectance;
+        // // amount of light that is reflected by the material
+        // vec3 reflectance;
 };
 
 // TESTING VARIABLES
@@ -58,14 +60,20 @@ float hgPhaseFunction(vec3 wi, vec3 wo) {
     return 1.0 / (4.0 * PI) * (1.0 - g * g) / pow(1.0 + g * g - 2.0 * g * cosTheta, 1.5);
 }
 
-float Rs(float cosTheta, float n1, float n2) {
-    return (n1 * cosTheta - n2 * sqrt(1.0 - (n1 / n2) * (n1 / n2) * (1.0 - cosTheta * cosTheta))) /
-           (n1 * cosTheta + n2 * sqrt(1.0 - (n1 / n2) * (n1 / n2) * (1.0 - cosTheta * cosTheta)));
+float Rs(float cosI, float cosT, float n1, float n2) {
+    float term1 = n1 * cosI - n2 * cosT;
+    float term2 = n1 * cosI + n2 * cosT;
+    if (term1 == 0.0 || term2 == 0.0) 
+        return 0.0;
+    return  term1/term2;
 }
 
-float Rp(float cosTheta, float n1, float n2) {
-    return (n1 * sqrt(1.0 - (n1 / n2) * (n1 / n2) * (1.0 - cosTheta * cosTheta)) - n2 * cosTheta) /
-           (n1 * sqrt(1.0 - (n1 / n2) * (n1 / n2) * (1.0 - cosTheta * cosTheta)) + n2 * cosTheta);
+float Rp(float cosI, float cosT, float n1, float n2) {
+    float term1 = n1 * cosT - n2 * cosI;
+    float term2 = n1 * cosT + n2 * cosI;
+    if (term1 == 0.0 || term2 == 0.0) 
+        return 0.0;
+    return  term1/term2;
 }
 
 // Trowbridge-Reitz GGX Normal Distribution Function
@@ -76,6 +84,7 @@ float Distribution_GGX(float NdotH, float roughness)
     float NdotH2 = NdotH*NdotH;
     float denom = (NdotH2 * (a2 - 1.0) + 1.0);
     denom = PI * denom * denom;
+    if (denom == 0.0) return 0.0;
     return a2 / denom;
 }
 
@@ -90,7 +99,9 @@ float G1(vec3 w, vec3 normal, float roughness)
     float cos2theta_w = dot(w, normal) * dot(w, normal);
     float sin2theta_w = 1.0 - cos2theta_w;
     float tan2theta_w = sin2theta_w / cos2theta_w;
-    return 1 / (1 + lambda(tan2theta_w, roughness));
+    float denom = (1 + lambda(tan2theta_w, roughness));
+    if (denom == 0.0) return 0.0;
+    return 1 / denom;
 }
 
 float G(vec3 wi, vec3 wo, vec3 normal, float roughness)
@@ -103,24 +114,15 @@ float G(vec3 wi, vec3 wo, vec3 normal, float roughness)
     float sin2theta_wo = 1.0 - cos2theta_wo;
     float tan2theta_wi = sin2theta_wi / cos2theta_wi;
     float tan2theta_wo = sin2theta_wo / cos2theta_wo;
-    return 1 / (1 + lambda(tan2theta_wi, roughness) + lambda(tan2theta_wo, roughness));
+    float denom = (1 + lambda(tan2theta_wi, roughness) + lambda(tan2theta_wo, roughness));
+    if (denom == 0.0) return 0.0;
+    return 1 / denom;
 }
 
 float D(vec3 w, vec3 h, vec3 normal, float roughness)
 {
-    float costheta_w = abs(dot(w, normal));
-    if (costheta_w == 0.0) return 0.0;
-    // return G1(w, roughness) / costheta_w * Distribution_GGX(dot(h, normal), roughness) * abs(dot(w, h), 0.0);
     return Distribution_GGX(dot(h, normal), roughness);
 }
-
-// float D_PDF(vec3 w, vec3 h, vec3 normal, float roughness)
-// {
-//     float costheta_w = abs(dot(w, normal));
-//     if (costheta_w == 0.0) return 0.0;
-//     return G1(w, normal, roughness) / costheta_w * Distribution_GGX(dot(h, normal), roughness) * abs(dot(w, h));
-//     // return Distribution_GGX(dot(h, normal), roughness);
-// }
 
 vec3 DiffuseReflectance(float n, vec3 sigma_s, vec3 sigma_a, float g)
 {
@@ -140,18 +142,21 @@ vec3 DiffuseReflectance(float n, vec3 sigma_s, vec3 sigma_a, float g)
     return Rd;
 }
 
-float FresnelReflection(float n, float cosTheta)
+float FresnelReflection(float n1, float n2, float cosT, float cosI)
 {
-    float Rs = Rs(cosTheta, 1.0, n);
-    float Rp = Rp(cosTheta, 1.0, n);
+    float Rs = Rs(cosI, cosT, n1, n2);
+    float Rp = Rp(cosI, cosT, n1, n2);
     float Fr = 0.5 * (abs(Rs * Rs) + abs(Rp * Rp));
     return Fr;
 }
 
 vec3 SingleScattering(vec3 albedo, float Fresnel, vec3 normal, vec3 wi, vec3 wo)
 {
-    vec3 single_scattering = albedo * Fresnel * hgPhaseFunction(normalize(wi), wo) / ( abs(dot(normal, normalize(wi))) + abs(dot(normal, wo)) );
-    return single_scattering;
+    vec3 nom = albedo * Fresnel * hgPhaseFunction(normalize(wi), wo);
+    float denom = abs(dot(normal, normalize(wi))) + abs(dot(normal, wo));
+    if (denom == 0.0) return vec3(0.0);
+    // vec3 single_scattering = albedo * Fresnel * hgPhaseFunction(normalize(wi), wo) / ( abs(dot(normal, normalize(wi))) + abs(dot(normal, wo)) );
+    return nom/denom;
 }
 
 void main()
@@ -163,7 +168,7 @@ void main()
     material.g = g;
     material.n = n_material;
     material.roughness = roughness;
-    material.reflectance = reflectance;
+    // material.reflectance = reflectance;
 
     // Calculating the albedo
     vec3 sigma_t = sigma_s + sigma_a;
@@ -196,28 +201,32 @@ void main()
     
 
         // Ft for in-scattering
-            
-            float Fr_1 = FresnelReflection(material.n, cos_incident);
+            float sin_incident = sqrt(1.0 - cos_incident * cos_incident);
+            float sin_refracted = sin_incident / material.n;
+            float cos_refracted = sqrt(1.0 - sin_refracted * sin_refracted);
+
+            float Fr_1 = FresnelReflection(1.0, material.n, cos_refracted, max(cos_incident, 0.0));
             float Ft_1 = 1.0 - Fr_1;
-            Ft_1 = max(Ft_1, 0.0);
+            // Ft_1 = max(Ft_1, 0.0);
         // Ft for out-scattering
             
-            float cos_refracted = dot(Fnormal, wo);
-            float sin_refracted = sqrt(1.0 - cos_refracted * cos_refracted);
+            float cos_refracted_2 = dot(Fnormal, wo);
+            float sin_refracted_2 = sqrt(1.0 - cos_refracted_2 * cos_refracted_2);
             //from material to air
-                float sin_incident = sin_refracted / material.n;
-                float cos_incident_2 = sqrt(1.0 - sin_incident * sin_incident);
-                float Fr_2 = FresnelReflection(1.0, cos_incident_2);
+                float sin_incident_2 = sin_refracted_2 / material.n;
+                float cos_incident_2 = sqrt(1.0 - sin_incident_2 * sin_incident_2);
+                float Fr_2 = FresnelReflection(material.n, 1.0, cos_refracted_2, max(cos_incident_2, 0.0));
+                Fr_2 = clamp(Fr_2, 0.0, 1.0);
                 float Ft_2 = 1.0 - Fr_2;
-                Ft_2 = max(Ft_2, 0.0);
+                // Ft_2 = max(Ft_2, 0.0);
         // full Fresnel term
             float Fresnel = Ft_1 * Ft_2;  
 
         //single scattering term
-        vec3 single_scattering = SingleScattering(material.albedo, Fresnel, Fnormal, wi, wo);
+        vec3 single_scattering = SingleScattering(material.albedo, Fresnel, Fnormal, wi, wo)*0.0;
 
         // Full BSSRDF approximation with BRDF
-        vec3 BSSRDF = single_scattering + Fresnel*DiffuseReflectance/PI;
+        vec3 BSSRDF = (single_scattering + Fresnel*DiffuseReflectance/PI) * max(cos_incident,0.0);
 
         //diffuse lighting
             // 1/PI is a normalization factor to ensure that total energy is conserved
@@ -225,36 +234,26 @@ void main()
 
         
 
-        // //specular lighting
-        vec3 halfwayDir = normalize(wi + wo);
-        // // using the GGX normal distribution function
-        // float NdotH = max(dot(Fnormal, halfwayDir), 0.0);
-        // float NDF = Distribution_GGX(NdotH, material.roughness);
-        // // float G = Geometry_Smith(dot(Fnormal, wo), dot(Fnormal, wi), roughness);
-        // float G = Geometry_Smith(wi, wo, roughness);
-        // // using Blinn-Phong
-        // // float specular = pow(max(dot(Fnormal, halfwayDir), 0.0), (2/(roughness*roughness)-2.0))* (1/(PI*roughness*roughness)) ;
+        // specular lighting
+            vec3 halfwayDir = normalize(wi + wo);
+            // Normal Distribution Function
+            float D = D(wi, halfwayDir, Fnormal, roughness);
+            // Geometry Shadowing Function
+            float G = G(wi, wo, Fnormal, roughness);
 
-        // // float numerator    = NDF * G * /*max(Fr_1,0.0) */ 1.0;
-        // // float denominator = 4.0 * max(dot(Fnormal, wo), 0.0) * max(dot(Fnormal, wi), 0.0);
+
+            float numerator = D * G * /*Fr_1*/ 1.0;
+            float denominator = 4.0 * abs(dot(Fnormal, wo))*abs(dot(Fnormal, wi));
+            float BRDF = 0.0;
+            // Avoid division by zero
+            if(denominator != 0.0) {
+                BRDF = numerator / denominator * max(cos_incident,0.0);
+            }
         
-        // Normal Distribution Function
-        float D = D(wi, halfwayDir, Fnormal, roughness);
-            // PDF and it's function is Not needed
-        // float PDF = D_PDF(wi, halfwayDir, Fnormal, roughness);
-        // Geometry Shadowing Function
-        float G = G(wi, wo, Fnormal, roughness);
+        resultFcolor += (BRDF + BSSRDF) * lightRadiance;
+        // resultFcolor += diffuse;
+        // resultFcolor += BRDF * lightRadiance;
 
-
-        float numerator = D*G  /* * Fresnel */;
-        float denominator = 4.0 * abs(dot(Fnormal, wo))*abs(dot(Fnormal, wi));
-        float BRDF = 0.0;
-        // Avoid division by zero
-        if(denominator != 0.0) {
-            BRDF = numerator / denominator * max(cos_incident,0.0);
-        }
-        
-        resultFcolor += BRDF*lightRadiance;
         
     }
     FragColor = vec4(resultFcolor, 1.0);
